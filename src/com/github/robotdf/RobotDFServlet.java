618c9fc604f57dd3c9a969882575a898a7ee07ce
Egon Willighagen <egon.willighagen@gmail.com>
2009-10-23 17:13:06 +0200

    Use Jena to do something... still struggling with org.slf4j.LoggerFactory not found in classpath :(

--- src/com/github/robotdf/RobotDFServlet.java
+++ src/com/github/robotdf/RobotDFServlet.java
@@ -1,5 +1,9 @@
 package com.github.robotdf;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,11 +15,13 @@ import com.google.wave.api.Range;
 import com.google.wave.api.RobotMessageBundle;
 import com.google.wave.api.TextView;
 import com.google.wave.api.Wavelet;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 @SuppressWarnings("serial")
 public class RobotDFServlet extends AbstractRobotServlet {
 
-	private final static String VERSION = "1";
+	private final static String VERSION = "2";
 	
 	Pattern pattern = Pattern.compile("owl:sameAs\\[.+\\]");
 
@@ -53,11 +59,31 @@ public class RobotDFServlet extends AbstractRobotServlet {
 				String replacement = match.substring("owl:sameAs[".length());
 				replacement = replacement.substring(0, replacement.length()-1);
 				if (replacement != null && replacement.length() > 0) {
-					textView.replace(
-						new Range(start, end),
-						replacement
-					);
-//					textView.setAnnotation(new Range(start, start+replacement.length()), "chem/molForm", replacement);
+					try {
+						URL realURL = new URL(replacement);
+				        URLConnection connection = realURL.openConnection();
+				        connection.setRequestProperty(
+				            "Accept",
+				            "application/xml, application/rdf+xml"
+				        );
+				        Model model = ModelFactory.createOntologyModel();
+				        model.read(connection.getInputStream(), "", "RDF/XML");
+						textView.replace(
+							new Range(start, end),
+							replacement + "[Triples found: " + model.size() + "]"
+						);
+//						textView.setAnnotation(new Range(start, start+replacement.length()), "chem/molForm", replacement);
+					} catch (MalformedURLException e) {
+						textView.replace(
+						    new Range(start, end),
+						    textView.getText() + "[Bad URL]"
+						);
+					} catch (IOException e) {
+						textView.replace(
+							new Range(start, end),
+							textView.getText() + "[IO Error: " + e.getMessage() + "]"
+						);
+					}
 				}
 			} else {
 				// OK, nothing more found, so return
